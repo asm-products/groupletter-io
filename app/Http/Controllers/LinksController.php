dcfa7f0a9a4aaa61a5483f7a3d0a9020f71d83da
Reinier Ladan <reinierl@gmail.com>
2015-02-24 21:25:52 +0100

    Links behind login

--- app/Http/Controllers/LinksController.php
+++ app/Http/Controllers/LinksController.php
@@ -8,6 +8,11 @@ use App\Link;
 
 class LinksController extends Controller {
 
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+
 	/**
 	 * Display a listing of the resource.
 	 *
